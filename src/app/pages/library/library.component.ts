3496c3200d573cc81606e1dc84618ef77e7d90cc
cedric <cedric.belot.dev@gmail.com>
2024-09-04 14:11:31 +0200

    exo 04

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Book} from "../../domain/book";
 import {ThumbsPipe} from "../../utils/pipes/thumbs.pipe";
+import {FormsModule} from "@angular/forms";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-library',
   standalone: true,
   imports: [
-    ThumbsPipe
+    ThumbsPipe,
+    FormsModule
   ],
   templateUrl: './library.component.html',
   styleUrl: './library.component.css'
@@ -18,8 +21,39 @@ export class LibraryComponent {
     {title: "title2", author: "author2", isRead: false},
     {title: "title3", author: "author3", isRead: false},
   ];
+  book: Book = {
+    title: "",
+    author: "",
+    isRead: false,
+  };
+  isSubmitted = false;
 
   toggleIsRead(book: Book): void {
     book.isRead = !book.isRead;
+  }
+
+  get titleHasError(): boolean {
+    return this.isSubmitted && this.book.title.length === 0;
+  }
+
+  get authorHasError(): boolean {
+    return this.isSubmitted && this.book.author.length === 0;
+  }
+
+  submitBook() {
+    this.isSubmitted = true;
+    if (!this.titleHasError && !this.authorHasError) {
+      this.library.push({...this.book});
+      this.resetForm();
+    }
+  }
+
+  private resetForm() {
+    this.isSubmitted = false;
+    this.book = {
+      title: "",
+      author: "",
+      isRead: false,
+    };
   }
 }
